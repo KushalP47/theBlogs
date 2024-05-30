8279562f2b70a58501fdfacf029b0cc82515cc84
Kushal Patel <kushal300603@gmail.com>
2024-05-31 00:03:56 +0530

    feat: Auth Functionality Added

--- src/App.jsx
+++ src/App.jsx
@@ -3,9 +3,10 @@
 function App() {
   return (
     <>
-      <div className="w-screen h-screen bg-white">
-        <h1 className='text-orange text-center text-5xl text-bolds p-4'>theBlogs</h1>
+      <div className="w-full h-40 bg-black flex flex-begin">
+        <img className="w-50 h-50 text-3xl" src="design/color/theblogs-high-resolution-logo-bg-dark.svg" alt="logo" />
       </div>
+      <div className="w-screen h-screen bg-white"></div>
     </>
   )
 }
---
+++ src/appwrite/auth.js
@@ -0,0 +1,60 @@
+import {Account, Client, ID} from 'appwrite';
+import conf from '../conf/conf.js';
+
+export class AuthServices {
+    client = new Client();
+    account;
+
+    constructor() {
+        this.client
+            .setEndpoint(conf.appwriteUrl)
+            .setProject(conf.appwriteProjectId);
+        
+        this.account = new Account(this.client);
+    }
+
+    async createAccount({ email, password, name }) {
+        try {
+            const userAccount = await this.account.create(ID.unique(), email, password, name);
+
+            if(userAccount) {
+                // call another method
+                return this.loginAccount({email, password});
+            } else {
+                return userAccount
+            }
+        } catch (error) {
+            console.log("AppWrite Error :: Create Account : ", error)
+            throw error;
+        }
+    }
+
+    async loginAccount({ email, password }){
+        try {
+            return await this.account.createEmailPasswordSession(email, password);
+        } catch (error) {
+            console.log("AppWrite Error :: Login Account : ", error)
+            throw error
+        }
+    }
+
+    async getCurrentUser() {
+        try {
+            return await this.account.get();
+        } catch (error) {
+            console.log("AppWrite Error :: Get Current Account : ", error)
+            throw error
+        }
+    }
+
+    async logoutAccount() {
+        try {
+            return await this.account.deleteSessions()
+        } catch (error) {
+            console.log("AppWrite Error :: Logout Account : ", error)
+            throw error
+        }
+    }
+}
+
+export const authService = new AuthServices();
