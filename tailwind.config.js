d1e611c08fd99017d8b8beaf9e0cd1e222d7cd62
Kushal Patel <kushal300603@gmail.com>
2024-05-30 18:37:02 +0530

    feat: Appwrite & ENV configuration

---
+++ tailwind.config.js
@@ -0,0 +1,17 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+    content: [
+      "./index.html",
+      "./src/**/*.{js,ts,jsx,tsx}",
+    ],
+    theme: {
+      colors: {
+        transparent: 'transparent',
+        current: 'currentColor',
+        'white': '#f9f4ec',
+        'black': '#201f1f',
+        'orange': '#ee5626'
+      },
+    },
+    plugins: [],
+  }
