40eb81ae5c20bf46c8806d8ea2b26820c38dc5cf
Kushal Patel <kushal300603@gmail.com>
2024-06-01 23:59:13 +0530

    feat: React Router Configured

--- src/components/AuthLayout.jsx
+++ src/components/AuthLayout.jsx
@@ -2,20 +2,32 @@ import React, {useEffect, useState} from 'react'
 import { useSelector } from 'react-redux'
 import { useNavigate } from 'react-router-dom'
 
-function Protected({children}) {
+function Protected({children, authentication = true}) {
+
     const navigate = useNavigate()
     const [loader, setLoader] = useState(true)
     const authStatus = useSelector(state => state.auth.status)
 
     useEffect(() => {
-        if(!authStatus) {
+        //TODO: make it more easy to understand
+
+        // if (authStatus ===true){
+        //     navigate("/")
+        // } else if (authStatus === false) {
+        //     navigate("/login")
+        // }
+        
+        //let authValue = authStatus === true ? true : false
+
+        if(authentication && authStatus !== authentication){
             navigate("/login")
-        } else {
+        } else if(!authentication && authStatus !== authentication){
             navigate("/")
         }
         setLoader(false)
-    }, [navigate, authStatus])
-    return loader ? <h1>Loading...</h1> : <>{children}</>
+    }, [authStatus, navigate, authentication])
+
+  return loader ? <h1>Loading...</h1> : <>{children}</>
 }
 
 export default Protected
